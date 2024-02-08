e4ae0c1ce59130373c2ab72b4991acb5af84df42
Elizabeth Viricheva <liza.vir@ya.ru>
2024-02-08 15:39:22 +0300

    added tests

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -50,4 +50,52 @@ public class TodosTest {
         Task[] expected = {simpleTask, epic, meeting};
         Task[] actual = todos.search("Позвонить");
     }
+
+    @Test
+    public void shouldSearchOneTaskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Написать сестре", "Позвонить сначала папе"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Позвонить друзьям",
+                "Пет-проект",
+                "В пятницу"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask };
+        Task[] actual = todos.search("Написать");
+    }
+
+    @Test
+    public void shouldSearchNoOneTaskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Написать сестре", "Позвонить сначала папе"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Позвонить друзьям",
+                "Пет-проект",
+                "В пятницу"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("55");
+    }
 }
