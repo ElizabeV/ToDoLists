5b795d38271cf7f1cd2e3c15cd51d5ef8ed79943
Elizabeth Viricheva <liza.vir@ya.ru>
2024-02-08 14:58:16 +0300

    check

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -6,10 +6,66 @@ public class TasksTest {
     @Test
     public void shouldCompareSimpleTaskWithTitle() {
         SimpleTask simpletask = new SimpleTask(1, "Сделать покупки");
-        //simpletask.matches("Сделать покупки");
         boolean expected = true;
         boolean actual = simpletask.matches("Сделать покупки");
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldNotCompareSimpleTaskWithTitle() {
+        SimpleTask simpletask = new SimpleTask(1, "Сделать");
+        boolean expected = false;
+        boolean actual = simpletask.matches("Сделать покупки");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCompareEpicWithSubtask() {
+        String[] tasks = {"Сделать покупки", "Собраться в дорогу"};
+        Epic epic = new Epic(1, tasks);
+        boolean expected = true;
+        boolean actual = epic.matches("Сделать покупки");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotCompareEpicWithSubtask() {
+        String[] tasks = {"Сделать", "Собраться в дорогу"};
+        Epic epic = new Epic(1, tasks);
+        boolean expected = false;
+        boolean actual = epic.matches("Сделать покупки");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCompareMeetingWithTopic() {
+        Meeting meeting = new Meeting(1, "Сделать покупки", "Дом", "После работы");
+        boolean expected = true;
+        boolean actual = meeting.matches("Сделать покупки");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotCompareMeetingWithTopic() {
+        Meeting meeting = new Meeting(1, "Сделать", "Дом", "После работы");
+        boolean expected = false;
+        boolean actual = meeting.matches("Сделать покупки");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCompareMeetingWithProject() {
+        Meeting meeting = new Meeting(1, "Сделать покупки", "Дом", "После работы");
+        boolean expected = true;
+        boolean actual = meeting.matches("Дом");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotCompareMeetingWithProject() {
+        Meeting meeting = new Meeting(1, "Сделать покупки", "Работа", "После работы");
+        boolean expected = false;
+        boolean actual = meeting.matches("Дом");
+        Assertions.assertEquals(expected, actual);
+    }
 }
--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -6,7 +6,7 @@ public class TodosTest {
     public void shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
-        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
         Epic epic = new Epic(55, subtasks);
 
         Meeting meeting = new Meeting(
@@ -22,8 +22,32 @@ public class TodosTest {
         todos.add(epic);
         todos.add(meeting);
 
-        Task[] expected = { simpleTask, epic, meeting };
+        Task[] expected = {simpleTask, epic, meeting};
         Task[] actual = todos.findAll();
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchThreeTypesOfQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Написать сестре", "Позвонить сначала папе"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Позвонить друзьям",
+                "Пет-проект",
+                "В пятницу"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.search("Позвонить");
     }
 }
